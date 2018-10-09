de05bf839409e7de2f26e6ea5627bf952e84fecd
Dmitrii Komarchev <komarchev@gmail.com>
2018-10-09 08:56:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
